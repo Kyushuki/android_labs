a4544d16bcdfeaa8bc7a8823f26d0a0dd59f27fb
Andrew <89681138+Kyushuki@users.noreply.github.com>
2026-04-26 23:33:57 +0300

    Lab 4

---
+++ composeApp/src/androidMain/kotlin/ui/theme/ColorScheme.android.kt
@@ -0,0 +1,26 @@
+package ui.theme
+
+import android.os.Build
+import androidx.compose.material3.ColorScheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.ui.platform.LocalContext
+
+@Composable
+@ReadOnlyComposable
+actual fun getApplicationColorScheme(useDarkTheme: Boolean): ColorScheme {
+    val context = LocalContext.current
+    val colors = when {
+        (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) -> {
+            if (useDarkTheme) dynamicDarkColorScheme(context)
+            else dynamicLightColorScheme(context)
+        }
+        useDarkTheme -> darkColorScheme()
+        else -> lightColorScheme()
+    }
+    return colors
+}
--- composeApp/src/commonMain/kotlin/com/example/android_labs/App.kt
+++ composeApp/src/commonMain/kotlin/com/example/android_labs/App.kt
@@ -36,16 +36,19 @@ import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.Card
 import androidx.compose.material3.Checkbox
+import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.semantics.SemanticsActions.OnClick
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import jdk.jfr.Description
 import org.jetbrains.compose.resources.StringResource
 import org.jetbrains.compose.resources.stringResource
-
+import ui.theme.getApplicationColorScheme
 
 
 data class ShoppingListItem (
@@ -68,79 +71,84 @@ fun ShoppingListElement(item: ShoppingListItem, onBoughtChange: (Boolean) -> Uni
 }
 @Composable
 fun App() {
-    Column {
-        Card(
-            modifier = Modifier.padding(8.dp)
-        ) {
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-                modifier = Modifier.padding(8.dp)
-            ) {
-                Image(
-                    painterResource(Res.drawable.compose_multiplatform),
-                    contentDescription = null,
-                    modifier = Modifier.size(64.dp)
-                )
-                Column {
-                    Text(
-                        stringResource(Res.string.lab_num),
-                        style = MaterialTheme.typography.titleSmall,
-                        overflow = TextOverflow.Ellipsis,
-                        maxLines = 1
+    MaterialTheme (colorScheme = getApplicationColorScheme()) {
+        Scaffold { contentPadding ->
+            Column (Modifier.padding((contentPadding))) {
+                Card(
+                    modifier = Modifier.padding(8.dp)
+                ) {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        modifier = Modifier.padding(8.dp)
+                    ) {
+                        Image(
+                            painterResource(Res.drawable.compose_multiplatform),
+                            contentDescription = null,
+                            modifier = Modifier.size(64.dp)
+                        )
+                        Column {
+                            Text(
+                                stringResource(Res.string.lab_num),
+                                style = MaterialTheme.typography.titleSmall,
+                                overflow = TextOverflow.Ellipsis,
+                                maxLines = 1
+                            )
+                            Text(
+                                stringResource(Res.string.lab_name),
+                                style = MaterialTheme.typography.bodySmall,
+                                overflow = TextOverflow.Ellipsis,
+                                maxLines = 1
+                            )
+                        }
+                    }
+                }
+                val exampleText1 = stringResource(Res.string.example, 1)
+                val exampleText2 = stringResource(Res.string.example, 2)
+                val exampleText3 = stringResource(Res.string.example, 3)
+                val shoppingList = rememberSaveable {
+                    mutableStateListOf(
+                        ShoppingListItem(exampleText1),
+                        ShoppingListItem(exampleText2),
+                        ShoppingListItem(exampleText3)
                     )
-                    Text(
-                        stringResource(Res.string.lab_name),
-                        style = MaterialTheme.typography.bodySmall,
-                        overflow = TextOverflow.Ellipsis,
-                        maxLines = 1
-                    )
+                }
+                var newItemDesc by rememberSaveable { mutableStateOf("")}
+                LazyColumn(Modifier.fillMaxWidth()) {
+                    item {
+                        OutlinedTextField(value = newItemDesc, onValueChange = {newItemDesc = it},
+                            modifier = Modifier.padding(8.dp),
+                            label = {
+                                Text(
+                                    stringResource(Res.string.addbtn)
+                                )
+                            },
+                            trailingIcon = {
+                                IconButton(onClick = {
+                                    if (newItemDesc.isNotBlank()) {
+                                        shoppingList.add(ShoppingListItem(newItemDesc.trim()))
+                                        newItemDesc = ""
+                                    }
+                                }) {
+                                    Icon(Icons.Default.Add, contentDescription = "Добавить")
+                                }
+                            })
+                    }
+                    itemsIndexed(shoppingList) { i, item ->
+                        ShoppingListElement(
+                            item,
+                            onBoughtChange = {
+                                shoppingList[i] = item.copy(bought = it)
+                            },
+                            onDelete = {
+                                shoppingList.removeAt(i)
+                            }
+                        )
+
+                    }
                 }
             }
         }
-        val exampleText1 = stringResource(Res.string.example, 1)
-        val exampleText2 = stringResource(Res.string.example, 2)
-        val exampleText3 = stringResource(Res.string.example, 3)
-        val shoppingList = remember {
-            mutableStateListOf(
-                ShoppingListItem(exampleText1),
-                ShoppingListItem(exampleText2),
-                ShoppingListItem(exampleText3)
-            )
-        }
-        var newItemDesc by remember { mutableStateOf("")}
-        LazyColumn(Modifier.fillMaxWidth()) {
-            item {
-                OutlinedTextField(value = newItemDesc, onValueChange = {newItemDesc = it},
-                modifier = Modifier.padding(8.dp),
-                label = {
-                    Text(
-                        stringResource(Res.string.addbtn)
-                    )
-                },
-                trailingIcon = {
-                    IconButton(onClick = {
-                        if (newItemDesc.isNotBlank()) {
-                            shoppingList.add(ShoppingListItem(newItemDesc.trim()))
-                            newItemDesc = ""
-                        }
-                    }) {
-                        Icon(Icons.Default.Add, contentDescription = "Добавить")
-                    }
-                })
-            }
-            itemsIndexed(shoppingList) { i, item ->
-                ShoppingListElement(
-                    item,
-                    onBoughtChange = {
-                        shoppingList[i] = item.copy(bought = it)
-                    },
-                    onDelete = {
-                        shoppingList.removeAt(i)
-                    }
-                )
-
-            }
-        }
     }
+
 }
 
